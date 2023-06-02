95ea1118608917010990bcb6acf081b8863400bc
NoSocAlgroc <albertmartin2000@gmail.com>
2023-06-02 13:57:59 +0200

    Join with lookups idealista

--- formatted_idealista.py
+++ formatted_idealista.py
@@ -8,30 +8,59 @@ files=[f for f in os.scandir(dir) if f.is_dir()]
 def reduce(a,b):
     return a[:4]+tuple(map(sum,zip(a[4:],b[4:])))
 
-joined=None
+joinedRDD=None
 
 for file in files:
-    rdd = spark.read.parquet(file.path).rdd
+    incomeRDD = spark.read.parquet(file.path).rdd
     year=file.name.split("_")[0]
 
     def idealistaChangeType(x):
-        strCols=["country","province","municipality","neighborhood"]
+        keyCols=["district","neighborhood"]
         numCols=["bathrooms","numPhotos","price","priceByArea","latitude","longitude","distance"]
         boolCols=["exterior","has360","has3DTour","hasLift","hasPlan","hasStaging","hasVideo","newDevelopment","showAddress","topNewDevelopment"]
-        return ((x["district"],
-                 year), 
-                tuple((x[c] if c in x else None for c in strCols))+
+        return (tuple((x[c] if c in x else "None" for c in keyCols))+tuple(year,),
                 tuple((float(x[c]) for c in numCols))+
                 tuple((x[c]==True for c in boolCols))+
                 tuple((1,))
                 )
-    rdd=rdd.map(idealistaChangeType)
+    incomeRDD=incomeRDD.map(idealistaChangeType)
 
-    rdd=rdd.reduceByKey(reduce)
+    incomeRDD=incomeRDD.reduceByKey(reduce)
 
-    if joined is None:
-        joined=rdd
+    if joinedRDD is None:
+        joinedRDD=incomeRDD
     else:
-        joined=joined.union(rdd)
-joined=joined.reduceByKey(reduce)
-joined=joined.mapValues(lambda x: x[:4]+tuple((v/x[-1] for v in x[4:-1]))+x[-1:])
+        joinedRDD=joinedRDD.union(incomeRDD)
+joinedRDD=joinedRDD.reduceByKey(reduce)
+joinedRDD=joinedRDD.mapValues(lambda x: tuple((v/x[-1] for v in x[:-1]))+x[-1:])
+
+#Lookups
+
+lookupDir="data/lookup_tables"
+distRDD = spark.read.json(os.path.join(lookupDir,"income_lookup_district.json")).rdd
+neigRDD = spark.read.json(os.path.join(lookupDir,"income_lookup_neighborhood.json")).rdd
+
+
+distRDD=distRDD.map(lambda x:((x['district'].lower(),),(x['_id'],x['district_name'])))
+neigRDD=neigRDD.map(lambda x:((x['neighborhood'].lower(),),(x['_id'],x['neighborhood_name'])))
+
+def flattenValues(x):
+    k,(v1,v2)=x
+    return (k,v1+v2) if v2 is not None else (k,(v1+(None,None)))
+def neighKey(x):#(d,n,y),(v)
+    k,v=x
+    return ((k[1],),(k[0],k[2])+v)#(n),(d,y,v)
+
+def distKey(x):#(n),(d,y,v,nID,nName)
+    k,v=x
+    return ((v[0],),(k[0],)+v[1:])#(d),(n,y,v,nID,nName)
+
+def fullKey(x):#(d),(n,y,v,nID,nName,dID,dName)
+    k,v=x
+    return (k+v)#(d,n,y,v,nID,nName,dID,dName)
+    #return ((v[-2],v[-4],v[1],v[2:-4],v[0],v[-3],k[0],v[-1]))#(dID,nID,y,v,n,nName,d,dName)
+
+
+joinedRDD=joinedRDD.map(neighKey).leftOuterJoin(neigRDD).map(flattenValues)
+joinedRDD=joinedRDD.map(distKey).leftOuterJoin(distRDD).map(flattenValues)
+joinedRDD=joinedRDD.map(fullKey)
