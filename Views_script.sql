74b1ebdeccbb2295638bd148e442558e9cf39bea
NoSocAlgroc <albertmartin2000@gmail.com>
2023-06-16 19:10:30 +0200

    Model and feature view

--- Views_script.sql
+++ Views_script.sql
@@ -1,6 +1,6 @@
-################## VISTAS
+-- VISTAS
 
-###### vistes per district
+-- vistes per district
 
 create or replace view idealista_district as
 select "districtID" as districtID, district_name, sum(bathrooms) as bathrooms, sum("numPhotos") as numPhotos, sum(price) as price, sum("priceByArea") as priceByArea, sum(latitude) as latitude, sum(longitude) as longitude, sum(distance) as distance, sum(exterior) as exterior, sum(has360) as has360, sum("has3DTour") as has3DTour, sum("hasLift") as hasLift, sum("hasPlan") as hasPlan, sum("hasStaging") as hasStaging, sum("hasVideo") as hasVideo, sum("newDevelopment") as newDevelopment, sum("showAddress") as showAddress, sum("topNewDevelopment") as topNewDevelopment, sum("count") as count   
@@ -25,7 +25,7 @@ group by "districtID", district_name
 
 
 
-###### vistes per neighborhood
+-- vistes per neighborhood
 
 create or replace view idealista_neighborhood as
 select "neighborhoodID"  , sum(bathrooms) as bathrooms, sum("numPhotos") as numPhotos, sum(price) as price, sum("priceByArea") as priceByArea, sum(latitude) as latitude, sum(longitude) as longitude, sum(distance) as distance, sum(exterior) as exterior, sum(has360) as has360, sum("has3DTour") as has3DTour, sum("hasLift") as hasLift, sum("hasPlan") as hasPlan, sum("hasStaging") as hasStaging, sum("hasVideo") as hasVideo, sum("newDevelopment") as newDevelopment, sum("showAddress") as showAddress, sum("topNewDevelopment") as topNewDevelopment, sum("count") as count   
@@ -48,7 +48,7 @@ group by "neighborhoodID"
 
 
 
-###### vistes per año
+-- vistes per año
 
 create or replace view income_year as
 select "year", sum(rfd) as rfd, sum(population) as population 
@@ -66,7 +66,7 @@ from idealista
 group by "year"
 
 
-###### vistes per año y neighborhood
+-- vistes per año y neighborhood
 
 create or replace view idealista_year_neighborhood as
 select "year", "neighborhoodID"  , sum(bathrooms) as bathrooms, sum("numPhotos") as numPhotos, sum(price) as price, sum("priceByArea") as priceByArea, sum(latitude) as latitude, sum(longitude) as longitude, sum(distance) as distance, sum(exterior) as exterior, sum(has360) as has360, sum("has3DTour") as has3DTour, sum("hasLift") as hasLift, sum("hasPlan") as hasPlan, sum("hasStaging") as hasStaging, sum("hasVideo") as hasVideo, sum("newDevelopment") as newDevelopment, sum("showAddress") as showAddress, sum("topNewDevelopment") as topNewDevelopment, sum("count") as count   
@@ -87,7 +87,7 @@ where "neighborhoodID" is not null
 group by "neighborhoodID", "year"
 
 
-###### vistes per año y district
+-- vistes per año y district
 
 create or replace view  idealista_year_district as
 select "year", "districtID" as districtID, district_name, sum(bathrooms) as bathrooms, sum("numPhotos") as numPhotos, sum(price) as price, sum("priceByArea") as priceByArea, sum(latitude) as latitude, sum(longitude) as longitude, sum(distance) as distance, sum(exterior) as exterior, sum(has360) as has360, sum("has3DTour") as has3DTour, sum("hasLift") as hasLift, sum("hasPlan") as hasPlan, sum("hasStaging") as hasStaging, sum("hasVideo") as hasVideo, sum("newDevelopment") as newDevelopment, sum("showAddress") as showAddress, sum("topNewDevelopment") as topNewDevelopment, sum("count") as count   
@@ -110,26 +110,7 @@ group by "districtID", district_name, "year"
 
 
 
-
-################## QUERIES
-##### districto
-
-select *
-from income_district i join price_district p on i."districtID" = p."districtID" join idealista_district i2 on i."districtID"  = i2."districtID" 
-
-##### vecindario
-
-select *
-from income_neighborhood i join price_neighborhood p on i."neighborhoodID" = p."neighborhoodID" join idealista_neighborhood i2 on i."neighborhoodID"  = i2."neighborhoodID" 
-
-###### año
-
-select *
-from income_year i join price_year p on i."year" = p."year" 
-
-
-
-# district information
+-- district information
 
 create or replace view district_information as
 select i.districtID, i.district_name, i.rfd as rfd, i.population as population, p.amount as amount, p.perMeter as perMeter, p.diffAmount as diffAmount, p.diffPerMeter as diffPerMeter, p."usedAmount" as usedAmount, p."usedPerMeter" as usedPerMeter,
@@ -141,7 +122,7 @@ join price_district p on i.districtID = p.districtID
 join idealista_district i2 on i.districtID = i2.districtID;
 
 
-# district information per year
+-- district information per year
 
 create or replace view district_information_per_year as
 select i.districtID, i.year as year, i.district_name, i.rfd as rfd, i.population as population, p.amount as amount, p.perMeter as perMeter, p.diffAmount as diffAmount, p.diffPerMeter as diffPerMeter, p."usedAmount" as usedAmount, p."usedPerMeter" as usedPerMeter
@@ -149,8 +130,10 @@ from income_year_district i
 join price_year_district p on i.districtID = p.districtID;
 
 
-
-
+--View with features for model training
+create or replace view idealista_model as
+select i.bathrooms,i."numPhotos" ,i.latitude ,i.longitude,i.distance ,i.exterior,i.has360,i."hasLift",i."has3DTour" ,i."hasPlan" ,i."hasStaging" , i."hasVideo" ,i."newDevelopment" , i."showAddress" ,i."topNewDevelopment" , pd.amount ,pd.permeter,pd."usedAmount" ,pd."usedPerMeter",id.rfd ,id.population ,i.price
+from idealista i join price_district pd on i."districtID" = pd.districtid join income_district id on i."districtID" = id.districtid ;
 
 
 
